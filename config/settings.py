a84e3554acb292d63fd645d3a747c7600649f93b
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-09-10 00:03:35 +0500

    fix lang

--- config/settings.py
+++ config/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'uz-uz'
+LANGUAGE_CODE = 'uz-Uz'
 
 LANGUAGES = [
     ('en', _('English')),
